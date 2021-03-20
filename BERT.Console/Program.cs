405451189b582cf97d8287793d07362f322db46e
Gerjan vlot <gerjanvlot@live.nl>
2021-03-20 14:02:30 +0100

    removed toplevel statements

--- BERT.Console/Program.cs
+++ BERT.Console/Program.cs
@@ -2,18 +2,28 @@
 using System.Text.Json;
 using Microsoft.ML.Models.BERT;
 
-var modelConfig = new BertModelConfiguration()
+namespace BERT.Console
 {
-    VocabularyFile = "Model/vocab.txt",
-    ModelPath = "Model/bertsquad-10.onnx"
-};
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var modelConfig = new BertModelConfiguration()
+            {
+                VocabularyFile = "Model/vocab.txt",
+                ModelPath = "Model/bertsquad-10.onnx"
+            };
 
-var model = new BertModel(modelConfig);
-model.Initialize();
+            var model = new BertModel(modelConfig);
+            model.Initialize();
 
-var (tokens, probability) = model.Predict(args[0], args[1]);
-Console.WriteLine(JsonSerializer.Serialize(new
-{
-    Probability = probability,
-    Tokens = tokens
-}));
+            var (tokens, probability) = model.Predict(args[0], args[1]);
+
+            System.Console.WriteLine(JsonSerializer.Serialize(new
+            {
+                Probability = probability,
+                Tokens = tokens
+            }));
+        }
+    }
+}
