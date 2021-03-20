bf1ed5ee2f01ae54d2f61b46eda83bff71ea5227
Gerjan vlot <gerjanvlot@live.nl>
2021-03-20 13:06:45 +0100

    Added console sample

---
+++ BERT.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.ML.Models.BERT;
+
+namespace BERT.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var modelConfig = new BertModelConfiguration()
+            {
+                VocabularyFile = "Model/vocab.txt",
+                ModelPath = "Model/bertsquad-10.onnx"
+            };
+
+            var model = new BertModel(modelConfig);
+            model.Initialize();
+
+            var prediction = model.Predict(args[0],
+                args[1]);
+
+            JsonSerializer.Serialize(prediction);
+        }
+    }
+}
