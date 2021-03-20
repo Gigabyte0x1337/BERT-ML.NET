30c99a33fe55f9e953579f95ec2baf2000fc2a34
Gerjan vlot <gerjanvlot@live.nl>
2021-03-20 13:13:55 +0100

    C# Toplevel program

--- BERT.Console/Program.cs
+++ BERT.Console/Program.cs
@@ -2,29 +2,18 @@
 using System.Text.Json;
 using Microsoft.ML.Models.BERT;
 
-namespace BERT.Console
+var modelConfig = new BertModelConfiguration()
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var modelConfig = new BertModelConfiguration()
-            {
-                VocabularyFile = "Model/vocab.txt",
-                ModelPath = "Model/bertsquad-10.onnx"
-            };
+    VocabularyFile = "Model/vocab.txt",
+    ModelPath = "Model/bertsquad-10.onnx"
+};
 
-            var model = new BertModel(modelConfig);
-            model.Initialize();
+var model = new BertModel(modelConfig);
+model.Initialize();
 
-            var prediction = model.Predict(args[0],
-                args[1]);
-
-            System.Console.WriteLine(JsonSerializer.Serialize(new
-            {
-                Probability = prediction.probability,
-                Tokens = prediction.tokens
-            }));
-        }
-    }
-}
+var (tokens, probability) = model.Predict(args[0], args[1]);
+Console.WriteLine(JsonSerializer.Serialize(new
+{
+    Probability = probability,
+    Tokens = tokens
+}));
